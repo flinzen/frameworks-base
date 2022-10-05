39fe6c8249f67fdd62c9c29729a22eb03d6badf1
Dmitry Shchapin <diman199806@gmail.com>
2022-10-05 19:24:26 +0500

    changed volume levels to 25

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -263,13 +263,13 @@ public class AudioService extends IAudioService.Stub {
         5,  // STREAM_VOICE_CALL
         7,  // STREAM_SYSTEM
         7,  // STREAM_RING
-        15, // STREAM_MUSIC
+        24, // STREAM_MUSIC
         7,  // STREAM_ALARM
         7,  // STREAM_NOTIFICATION
-        15, // STREAM_BLUETOOTH_SCO
+        24, // STREAM_BLUETOOTH_SCO
         7,  // STREAM_SYSTEM_ENFORCED
-        15, // STREAM_DTMF
-        15  // STREAM_TTS
+        24, // STREAM_DTMF
+        24  // STREAM_TTS
     };
 
     /** Minimum volume index values for audio streams */
